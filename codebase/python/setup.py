1a163e2c7e7ba282edbe77dc631649cc46b9be1d
AndrewCGreen <andrewc.green@mac.com>
2020-02-28 12:06:31 -0500

    fixing env issue

--- codebase/python/setup.py
+++ codebase/python/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="python",
-        version="0.0.1",
-        packages=find_packages()
+    name="python",
+    version="0.0.1",
+    packages=find_packages(),
+#    package_data={"": "lib/libsfencrypt.so"},
+    include_package_data=True
 )
